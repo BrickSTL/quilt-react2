d9f70b6e6178c53a9b2bee80c875a3ffbff20283
Ben Merrill <bendmerrill@gmail.com>
2018-11-01 12:51:44 -0500

    Updates

---
+++ precache-manifest.7a7d6fbc8dbc9767caa94824ba9f349a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3877f9b99f4c8ba8e8c7",
+    "url": "/quilt-react2/static/js/runtime~main.3877f9b9.js"
+  },
+  {
+    "revision": "daabacc779d16e9370cd",
+    "url": "/quilt-react2/static/js/main.daabacc7.chunk.js"
+  },
+  {
+    "revision": "0f59d6f7a51524815c35",
+    "url": "/quilt-react2/static/js/1.0f59d6f7.chunk.js"
+  },
+  {
+    "revision": "daabacc779d16e9370cd",
+    "url": "/quilt-react2/static/css/main.1ee2ccbe.chunk.css"
+  },
+  {
+    "revision": "70a2cfd9c9c6f0306f4ffa4131dbe62f",
+    "url": "/quilt-react2/index.html"
+  }
+];
