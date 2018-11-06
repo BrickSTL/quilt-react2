196add9276b20173c730013e6a7a27f698f7c7fa
Joshua Aguilar <joshuaaguilar20@gmail.com>
2018-11-06 11:16:45 -0800

    Removed ArtFrame
    Make new Comp for Art Frame and pass props to art frame

--- src/components/News/NewSingle.js
+++ src/components/News/NewSingle.js
@@ -5,22 +5,7 @@ import React from 'react';
   const NewSingle = ({item}) => {
     let source = item.source.name;
     let image = item.urlToImage;
-    let artFrame = (
-      <a href={item.url} target='_blank'>
-        <div className="card-image">
-          <img src={item.urlToImage}/>
-        </div>
-        <div className="article-text text-left">
-        <span style={{fontSize:'22px'}} className='row'>{item.title}</span>
-        <span className='row'><p className='sourceDisplay'>{item.source.name}</p></span>
-        <span className='row'>{item.author}</span>
-        <span className='row'>{item.publishedAt}</span>
-        <p className="row"><i className="fa fa-save"></i> Save Article</p>
-
-        </div>
-      </a>
-    )
-
+    
     if(source==='BBC News'){
       return (
         <div className="element-item red1 newsArticle news-box col-md-4 col-sm red1Link">
